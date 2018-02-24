c640ec98e3afde7aae4449bf1a7bf801e28d3717
Guigo <guigo2k@guigo2k.com>
2018-02-24 16:28:28 -0300

    Add dotenv module

--- modules/config.js
+++ modules/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   rethinkdb: {
-    host: 'localhost',
-    port: 28015,
-    authKey: '',
-    db: 'game'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    db: process.env.DB_NAME,
+    authKey: process.env.TOKEN_SECRET
   },
   express: {
-     port: 3000
+     port: process.env.SERVER_PORT
   }
 };
